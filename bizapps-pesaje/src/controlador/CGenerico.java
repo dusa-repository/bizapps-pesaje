df23cb79eb6125d079afdc344e9120004a6c5f11
linkin9014@gmail.com <linkin9014@gmail.com@e0a162bf-abcc-ac32-ff54-6f35ec01d4e5>
2015-02-26 16:48:41 +0000

    Validado de session,,,

--- bizapps-pesaje/src/controlador/CGenerico.java
+++ bizapps-pesaje/src/controlador/CGenerico.java
@@ -12,38 +12,50 @@ import servicio.maestros.SConductor;
 import servicio.maestros.SProducto;
 import servicio.maestros.STransporte;
 import servicio.maestros.SVehiculo;
+import servicio.seguridad.SUsuario;
 import servicio.transacciones.SPesaje;
 
 public abstract class CGenerico implements ActionListener {
 
-	private static ApplicationContext applicationContext = new ClassPathXmlApplicationContext(
+	protected static ApplicationContext applicationContext = new ClassPathXmlApplicationContext(
 			"/META-INF/ConfiguracionAplicacion.xml");
 
 	public static SProducto getSProducto() {
 		return applicationContext.getBean(SProducto.class);
 	}
+
 	public static STransporte getSTransporte() {
 		return applicationContext.getBean(STransporte.class);
 	}
+
 	public static SAlmacen getSAlmacen() {
 		return applicationContext.getBean(SAlmacen.class);
 	}
+
 	public static SBalanza getSBalanza() {
 		return applicationContext.getBean(SBalanza.class);
 	}
+
 	public static SConductor getSConductor() {
 		return applicationContext.getBean(SConductor.class);
 	}
+
 	public static SVehiculo getSVehiculo() {
 		return applicationContext.getBean(SVehiculo.class);
 	}
+
 	public static SPesaje getSPesaje() {
 		return applicationContext.getBean(SPesaje.class);
 	}
+
+	public static SUsuario getSUsuario() {
+		return applicationContext.getBean(SUsuario.class);
+	}
+
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		cargarListener(arg0);
 	}
-	
+
 	public abstract void cargarListener(ActionEvent arg0);
 }
--- bizapps-pesaje/src/vistas/Index.java
+++ bizapps-pesaje/src/vistas/Index.java
@@ -9,13 +9,19 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JComboBox;
+
 import java.awt.Color;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 
 public class Index extends JFrame {
 
-	private JPanel contentPane;
-	private JTextField textField;
+	JPanel contentPane;
+	JTextField txtNombre;
+	JTextField txtPassword;
+	JButton btnLoggin;
+	JLabel lblLogin;
 
 	/**
 	 * Launch the application.
@@ -38,36 +44,73 @@ public class Index extends JFrame {
 	 */
 	public Index() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 536, 421);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
-		JLabel lblNewLabel = new JLabel("New label");
-		lblNewLabel.setBounds(22, 27, 46, 14);
+
+		JLabel lblNewLabel = new JLabel("Usuario");
+		lblNewLabel.setBounds(82, 129, 46, 14);
 		contentPane.add(lblNewLabel);
-		
-		textField = new JTextField();
-		textField.setBounds(78, 24, 86, 20);
-		contentPane.add(textField);
-		textField.setColumns(10);
-		
-		JLabel lblNewLabel_1 = new JLabel("New label");
-		lblNewLabel_1.setBounds(22, 64, 46, 14);
+
+		lblLogin = new JLabel("");
+		lblLogin.setForeground(Color.RED);
+		lblLogin.setBounds(275, 129, 206, 45);
+		contentPane.add(lblLogin);
+
+		txtNombre = new JTextField();
+		txtNombre.setBounds(138, 126, 86, 20);
+		contentPane.add(txtNombre);
+		txtNombre.setColumns(10);
+
+		btnLoggin = new JButton("Entrar");
+		btnLoggin.setBounds(135, 231, 89, 23);
+		contentPane.add(btnLoggin);
+
+		JLabel lblNewLabel_1 = new JLabel("Password");
+		lblNewLabel_1.setBounds(82, 175, 46, 14);
 		contentPane.add(lblNewLabel_1);
-		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(78, 61, 86, 20);
-		contentPane.add(comboBox);
-		
-		JLabel lblNewLabel_2 = new JLabel("New label");
-		lblNewLabel_2.setForeground(Color.RED);
-		lblNewLabel_2.setBounds(22, 124, 402, 45);
-		contentPane.add(lblNewLabel_2);
-		
-		JButton btnNewButton = new JButton("New button");
-		btnNewButton.setBounds(22, 180, 89, 23);
-		contentPane.add(btnNewButton);
+
+		txtPassword = new JTextField();
+		txtPassword.setBounds(138, 172, 86, 20);
+		contentPane.add(txtPassword);
+		txtPassword.setColumns(10);
+	}
+
+	public void agregarListener(ActionListener cIndex) {
+		btnLoggin.addActionListener(cIndex);
+	}
+
+	public JTextField getTxtNombre() {
+		return txtNombre;
+	}
+
+	public void setTxtNombre(JTextField txtNombre) {
+		this.txtNombre = txtNombre;
+	}
+
+	public JTextField getTxtPassword() {
+		return txtPassword;
+	}
+
+	public void setTxtPassword(JTextField txtPassword) {
+		this.txtPassword = txtPassword;
+	}
+
+	public JButton getBtnLoggin() {
+		return btnLoggin;
+	}
+
+	public void setBtnLoggin(JButton btnLoggin) {
+		this.btnLoggin = btnLoggin;
+	}
+
+	public JLabel getLblLogin() {
+		return lblLogin;
+	}
+
+	public void setLblLogin(JLabel lblLogin) {
+		this.lblLogin = lblLogin;
 	}
 }
